1390128307fcc9fc26eea5681aa13748325adad9
IronJoo <joanaferro92@gmail.com>
2021-03-11 21:12:23 +0000

    Exercise 3

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
-<?php
-echo nl2br("Tomorrow I'll learn PHP global variables.\nThis is a bad command : del c:\\*.*");
-?>
+<?php $var = "PHP Tutorial"?>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset=utf-8>
+  <title><?php echo $var; ?></title>
+</head>
+<body>
+    <h3><?php echo $var; ?></h3>
+    <p>PHP, an acronym for Hypertext Preprocessor, is a widely-used open source general-purpose scripting language. It is a cross-platform, HTML embedded server-side scripting language and is especially suited for web development.</p>
+    <p><a href="https://www.w3resource.com/php/php-home.php">Go to the <?php echo $var ?>.</a></p>
+</body>
+</html>
